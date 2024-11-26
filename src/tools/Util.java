e97573bd4e8b4976193fc609fd09bad7e18dd0aa
LucaStern <lucasternoliveira@gmail.com>
2024-11-26 19:24:59 -0400

    Criei as telas de cadastros, útil e os pacotes

---
+++ src/tools/Util.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tools;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author u09723876175
+ */
+public class Util {
+    public static void habilitar( boolean valor, JComponent ... comp){
+        for (int i = 0; i < comp.length; i++) {
+            comp[i].setEnabled(valor);
+        }
+    };
+    public static void limpar(JComponent... comp) {
+        for (int i = 0; i < comp.length; i++) {
+            JComponent c = comp[i];
+            if (c instanceof JTextField) {
+                ((JTextField) c).setText("");
+            } else if (c instanceof JCheckBox) {
+                ((JCheckBox) c).setSelected(false);
+            } else if (c instanceof JComboBox) {
+                ((JComboBox<?>) c).setSelectedIndex(0);
+            }
+        }
+    }
+}
